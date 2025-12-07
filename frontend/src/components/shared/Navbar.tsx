a4ac14c85805c7f708be39022edf92c30bff46f4
Figo Aljufrie <figoaldjoeffri@gmail.com>
2025-12-07 23:04:01 +0700

    fix: image

--- frontend/src/components/shared/Navbar.tsx
+++ frontend/src/components/shared/Navbar.tsx
@@ -28,8 +28,8 @@ const Navbar = () => {
   // Logo path
   const logoSrc =
     currentTheme === "dark"
-      ? "/src/assets/warung-white.svg"
-      : "/src/assets/warung-black.svg";
+      ? "warung-white.svg"
+      : "warung-black.svg";
   return (
     <motion.nav
       initial={{ y: -100, opacity: 0 }}
